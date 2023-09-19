bca2fd1d8ced994a1bc11a2d492bbc9636deee27
Ruchik Jani <jani.ru@northeastern.edu>
2023-09-19 02:05:54 +0000

    Upload New File

---
+++ PA1/publish.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# license removed for brevity
+import rospy
+from std_msgs.msg import Int16
+
+def publisher():
+    pub = rospy.Publisher('numeric_out', Int16, queue_size=10)
+    rospy.init_node('int_publish')
+    rate = rospy.Rate(10) 
+    while not rospy.is_shutdown():
+        num = 36
+        pub.publish(num)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        pass
